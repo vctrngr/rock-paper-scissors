ba86da877a9a4183f4dda68d23222b3437e5a55b
vctrngr <ctorv.ictor@anche.no>
2023-09-27 19:16:37 +0100

    Add buttons, display text

--- script.js
+++ script.js
@@ -9,77 +9,107 @@ function getPlayerChoice(){
     return choice.toLowerCase()
 }
 
-function playRound(playerSelection, computerSelection){
+const resultsArea = document.querySelector('.results');
 
+function playRound(playerSelection, computerSelection){
+    const info = document.createElement('p');
     let winner;
 
     if (playerSelection == computerSelection){
-        console.log('tie!!!')
+        info.textContent = 'tie!!!';
+        // console.log('tie!!!')
+        resultsArea.appendChild(info)
     }
     else if (playerSelection == 'rock'){
         if (computerSelection == 'paper'){
             winner = 'computer';
-            console.log('computer wins! Paper beats rock')
+            info.textContent = 'computer wins! Paper beats rock';
+            resultsArea.appendChild(info)
+            // console.log('computer wins! Paper beats rock')
         }
         else if (computerSelection == 'scissors'){
             winner = 'player';
-            console.log('you win! Rock beats scissors')
+            info.textContent = 'you win! Rock beats scissors';
+            resultsArea.appendChild(info)
+            // console.log('you win! Rock beats scissors')
         }
     }
         else if (playerSelection == 'paper'){
             if (computerSelection == 'scissors'){
                 winner = 'computer';
-                console.log('computer wins! Scissors beats paper')
+                info.textContent = 'computer wins! Scissors beats paper';
+                resultsArea.appendChild(info)
+                // console.log('computer wins! Scissors beats paper')
             }
         else if (computerSelection == 'rock'){
             winner = 'player';
-            console.log('you win! Paper beats rock')
+            info.textContent ='you win! Paper beats rock';
+            resultsArea.appendChild(info)
+            // console.log('you win! Paper beats rock')
         }
         }
     else if (playerSelection == 'scissors'){
         if (computerSelection == 'paper'){
             winner = 'player';
-            console.log('you win! Scissors beats paper')
+            info.textContent = 'you win! Scissors beats paper';
+            resultsArea.appendChild(info)
+            // console.log('you win! Scissors beats paper')
         }
         else if (computerSelection == 'rock'){
             winner = 'computer';
-            console.log('computer wins! Rock beats scissors')
+            info.textContent = 'computer wins! Rock beats scissors';
+            resultsArea.appendChild(info)
+            // console.log('computer wins! Rock beats scissors')
         }
     }
     return winner
 }
 
+
+function playRoundButton(e){
+    // console.log(e.target.id)
+    playRound(e.target.id, getComputerChoice())
+}
+
+const butt = document.querySelectorAll('button');
+
+butt.forEach(button => 
+    button.addEventListener('click', playRoundButton));
+
+
+
+
 // console.log(getPlayerChoice())
 
 
 
-function game(numRounds = 5){
-    let computerScore = 0, playerScore = 0;
+// function game(numRounds = 5){
+//     let computerScore = 0, playerScore = 0;
 
-    for (let i = 0; i < numRounds; i++){
-       let round = playRound(getPlayerChoice(), getComputerChoice());
-       if (round == 'computer'){
-        computerScore++;
-       }
-       else if (round == 'player'){
-        playerScore++
-       }
-       else{
-       }
-       console.log(computerScore)
-       console.log(playerScore)
-    }
+//     for (let i = 0; i < numRounds; i++){
+//        let round = playRound(getPlayerChoice(), getComputerChoice());
+//        if (round == 'computer'){
+//         computerScore++;
+//        }
+//        else if (round == 'player'){
+//         playerScore++
+//        }
+//        else{
+//        }
+//        console.log(computerScore)
+//        console.log(playerScore)
+//     }
 
-    if (computerScore > playerScore){
-        console.log('computer wins the game')
-    }
-    else if (computerScore < playerScore){
-        console.log('you win the game')
-    }
-    else{
-        console.log('it\'s a tie!')
-    }
-}
+//     if (computerScore > playerScore){
+//         console.log('computer wins the game')
+//     }
+//     else if (computerScore < playerScore){
+//         console.log('you win the game')
+//     }
+//     else{
+//         console.log('it\'s a tie!')
+//     }
+// }
 
-game()
+// game()
 
